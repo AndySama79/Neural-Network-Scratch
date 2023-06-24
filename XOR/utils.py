049c5210e5ddccb0c503abb26d94becd1c224350
AndySama79 <arnab.dey@plaksha.edu.in>
2023-06-24 22:53:25 +0530

    added utils.py and xor.py

--- XOR/utils.py
+++ XOR/utils.py
@@ -7,3 +7,17 @@ class TanH(Activation):
         tanh = lambda x: np.tanh(x)
         tanh_prime = lambda x: 1 - np.power(np.tanh(x), 2)
         super().__init__(tanh, tanh_prime)
+
+#*  Sigmoid
+class Sigmoid(Activation):
+    def __init__(self):
+        sigmoid = lambda x: 1 / (1 + np.exp(-x))
+        sigmoid_prime = lambda x: sigmoid(x) - np.power(sigmoid(x), 2)
+        super().__init__(sigmoid, sigmoid_prime)
+
+#*  ReLU
+class Relu(Activation):
+    def __init__(self):
+        relu = lambda x: np.maximum(x, 0)
+        relu_prime = lambda x: x > 0
+        super().__init__(relu, relu_prime)
